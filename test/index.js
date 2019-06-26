a4960c5ff1349ac6f997ad426f5bfffbdfddfe12
Mantong Zhao <zhaomantong0203@gmail.com>
2019-06-26 11:15:25 -0700

    update

---
+++ test/index.js
@@ -0,0 +1,26 @@
+// console.log('Hi, my name is Chris and I\'m 29 years old');
+// function getYearOfBirth(age){
+//   return 2019-age;
+// }
+// function createGreeting (name,age){
+//   if(age<0) {
+//     throw new Error('Age can not be negative');
+//   }
+//   if (name===undefined || age===undefined){
+//     throw new Error('Arguments not valid');
+//   }
+//   if (typeof name!=='string'&& typeof age !== 'number'){
+//     throw new TypeError('type error');
+//   }
+//   const yearOfBirth=2019-age;
+//   return `I was borin in ${yearOfBirth}.
+//   Hi, my name is ${name} and I'm ${age} years old`;
+// }
+// try{const greeting1 = createGreeting();
+// }catch (e){
+//   console.error(e);
+// }
+// const greeting1 = createGreeting();
+// console.log(greeting1);
+
+
